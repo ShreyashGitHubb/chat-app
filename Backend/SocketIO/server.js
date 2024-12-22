a26823542771c9bdb9e4ab216207cd178ed2f4f5
shreyash vishwakarma <shreyasvishwakarma00@gmail.com>
2024-12-22 12:46:02 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-backend-cppi.onrender.com",
+    origin: "https://shreyash-chat-app.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
